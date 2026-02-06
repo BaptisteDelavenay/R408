e4c440f81c4f1e5564188896e642a894d9349a96
Baptiste Delavenay <bdelavenay78@gmail.com>
2026-02-06 22:37:18 +0100

    première version de la classe clicker

--- JS/main.js
+++ JS/main.js
@@ -10,3 +10,19 @@
 // })
 
 
+class clicker {
+
+    constructor(){
+        this.compteur = 0
+    }
+
+    getCompteur(){
+        return this.compteur
+    }
+
+    incrementer(val){
+        this.compteur += val
+        return this.compteur
+    }
+
+}
