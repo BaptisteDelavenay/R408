2a0decf637f3d66f62d9851c177c9a4e3e475e8d
Baptiste Delavenay <bdelavenay78@gmail.com>
2026-02-06 22:30:49 +0100

    passer d'un script classique à une POO

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,12 @@
-let compteur = 0;
+// let compteur = 0;
 
-let button = document.getElementById("button");
-let afficheNB = document.getElementById("compteur");
+// let button = document.getElementById("button");
+// let afficheNB = document.getElementById("compteur");
 
-button.addEventListener("click", function(){
-    compteur+=1;
-    afficheNB.innerHTML = compteur
+// button.addEventListener("click", function(){
+//     compteur+=1;
+//     afficheNB.innerHTML = compteur
 
-})
+// })
+
+
