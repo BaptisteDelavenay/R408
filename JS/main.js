141b4e0565f7709d03fb97e7b7e4d71ad7e988fd
NoahCG <chrisnoa@univ-rouen.fr>
2026-02-10 14:27:38 +0100

    Ajout de tout les autres loup2

--- JS/main.js
+++ JS/main.js
@@ -22,6 +22,10 @@ let clicker = new Clicker();
 let button = document.getElementById("button");
 let afficheNB = document.getElementById("compteur");
 let loup = document.getElementById("loup");
+let alpha = document.getElementById("alpha");
+let loupia = document.getElementById("loupia");
+let tralelouloup = document.getElementById("tralelouloup");
+let loup67 = document.getElementById("loup67");
 
 
 button.addEventListener("click", function () {
