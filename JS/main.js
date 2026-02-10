99e6c13688c44b0500f695372e74cc4caff92f78
Baptiste Delavenay <bdelavenay78@gmail.com>
2026-02-10 14:29:04 +0100

    ajout event listener des boutons

--- JS/main.js
+++ JS/main.js
@@ -21,10 +21,16 @@ class Clicker {
 let clicker = new Clicker();
 let button = document.getElementById("button");
 let afficheNB = document.getElementById("compteur");
-let loup = document.getElementById("loup");
 
+let loup = document.getElementById("loup");
 
 button.addEventListener("click", function () {
   clicker.incrementer(1);
   afficheNB.innerHTML = clicker.getCompteur();
 });
+
+loup.addEventListener("click", function () {});
+alpha.addEventListener("click", function () {});
+louia.addEventListener("click", function () {});
+tralelouloup.addEventListener("click", function () {});
+lou67.addEventListener("click", function () {});
